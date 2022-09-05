8c3b0669f70b33676dde30fe840fefab4f605314
sam <bossley.samuel@gmail.com>
2022-09-04 20:00:51 -0700

    add screen locker

--- user/home.nix
+++ user/home.nix
@@ -151,6 +151,8 @@ in
     home.file.".config/wofi/config".source = ./config/wofi/config;
     home.file.".config/wofi/style.css".source = ./config/wofi/style.css;
 
+    home.file.".config/swaylock/config".source = ./config/swaylock/config;
+
     # }}}
 
     # theming {{{
