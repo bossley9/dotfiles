1acf270192c09160b8a3c4a8f18b1007ce181f38
sam <bossley.samuel@gmail.com>
2022-12-19 15:17:04 -0800

    update sn to 1.12.0

--- user/derivations/sn.nix
+++ user/derivations/sn.nix
@@ -2,26 +2,25 @@ with (import <nixpkgs> {});
 
 buildGoModule rec {
   pname = "sn";
-  version = "1.10.0";
+  version = "1.12.0";
 
   src = fetchFromSourcehut {
     owner = "~bossley9";
     repo = "${pname}";
     rev = "v${version}";
-    sha256 = "0wcx2jiblijmq8km9li2zlif6936d569nkfnhbdkgrbqv0g45bl4";
+    sha256 = "1bqdmrv633l9rvfhjg1ixaids8zf9p9zfgljwzmrhvqdjya51hpx";
   };
   tags = [ "production" ];
 
-  vendorSha256 = "sha256-/roVpjcqsUXR2bA1cTQ2tkBMl8MEczKM+HZbA6Mi6VU=";
+  vendorSha256 = "sha256-rg6C4g1yRta5MeUPgPjLyEKV+xRZJTxO9WaJXanX2Bw=";
 
   meta = with lib; {
-    description = "A Simplenote static-syncning CLI client written in Go";
+    description = "A Simplenote syncing CLI client written in Go";
     homepage = "https://git.sr.ht/~bossley9/sn";
     maintainers = with maintainers; [ bossley9 ];
     license = licenses.bsd2;
   };
 
-  # adapted from https://github.com/NixOS/nixpkgs/blob/00a89d15ab5a0c14c20c3e8784a413ab37d8bd9f/pkgs/build-support/go/module.nix#L288
   installPhase = ''
     mkdir -p $out/bin
     exe="$GOPATH/bin/sn"
