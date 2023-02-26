27400d94ffb7192e7b9248ae83920cbe24618952
Sam Bossley <bossley.samuel@gmail.com>
2023-02-25 19:28:25 -0800

    feat: convert vars to lua

--- user/config/nvim/lua/vars.lua
+++ user/config/nvim/lua/vars.lua
@@ -1,15 +1,13 @@
-vim.cmd([[
-  let g:openedWithDir = 0
+vim.g.openedWithDir = 0
 
-  if argc() > 0 && isdirectory(argv(0))
-    exe 'cd ' . argv(0)
-    let g:openedWithDir = 1
-  endif
-  if argc() == 0
-    let g:openedWithDir = 1
-  endif
+local argc = vim.fn.argc()
 
-  " resolve() resolves any symbolic links.
-  " fnameescape() escapes paths with spaces, e.g. "My\ Docs/".
-  let g:projectDir = fnameescape(resolve(getcwd()))
-]])
+if argc == 1 and vim.fn.isdirectory(vim.fn.argv(0)) ~= 0 then
+    vim.cmd.cd(vim.fn.argv(0))
+    vim.g.openedWithDir = 1
+end
+if argc == 0 then vim.g.openedWithDir = 1 end
+
+-- resolve() resolves any symbolic links.
+-- fnameescape() escapes paths with spaces, e.g. "My\ Docs/".
+vim.g.projectDir = vim.fn.fnameescape(vim.fn.resolve(vim.fn.getcwd()))
