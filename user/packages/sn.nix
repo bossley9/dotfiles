7b7c1d19ba1e1f4cd5fa62a55bc7c553abc1d17c
Sam Bossley <bossley.samuel@gmail.com>
2023-05-01 16:59:46 -0700

    feat: update sn derivation

--- user/packages/sn.nix
+++ user/packages/sn.nix
@@ -1,22 +1,22 @@
-{ buildGoModule, fetchFromSourcehut, lib, ... }:
+{ buildGoModule, fetchFromGitHub, lib, ... }:
 
 buildGoModule rec {
   pname = "sn";
-  version = "1.15.0";
+  version = "1.15.1";
 
-  src = fetchFromSourcehut {
-    owner = "~bossley9";
+  src = fetchFromGitHub {
+    owner = "bossley9";
     repo = "${pname}";
     rev = "v${version}";
-    sha256 = "1xjrbmb2rklxxwibk26qs01f1rk4llxy4iddj4r6i8azy55qyr7h";
+    sha256 = "070ggn0vva3s5qwj21pljh5zaya9fgfb3qivg5kl6idbk7mspacr";
   };
   tags = [ "production" ];
 
-  vendorSha256 = "sha256-rg6C4g1yRta5MeUPgPjLyEKV+xRZJTxO9WaJXanX2Bw=";
+  vendorSha256 = "sha256-2SmOOv66IfSR3bYBz8CaCKplUN3qyADbl79Wgd6zfds=";
 
   meta = with lib; {
     description = "A Simplenote syncing CLI client written in Go";
-    homepage = "https://git.sr.ht/~bossley9/sn";
+    homepage = "https://github.com/bossley9/sn";
     maintainers = with maintainers; [ bossley9 ];
     license = licenses.bsd2;
   };
