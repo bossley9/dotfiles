3cbfd0bf8b51648deaee343c10b96ba58ff4a038
Sam Bossley <bossley.samuel@gmail.com>
2023-05-03 16:36:12 -0700

    fix: confine sway workspaces to specific monitors

--- machines/bastion/display.nix
+++ machines/bastion/display.nix
@@ -23,6 +23,17 @@ in
           transform normal
         }
         exec_always wlr-randr --output DP-1 --preferred
+
+        workspace 1 output "DP-1"
+        workspace 2 output "DP-1"
+        workspace 3 output "DP-1"
+        workspace 4 output "DP-1"
+        workspace 5 output "DP-1"
+        workspace 6 output "HDMI-A-1"
+        workspace 7 output "HDMI-A-1"
+        workspace 8 output "HDMI-A-1"
+        workspace 9 output "HDMI-A-1"
+        workspace 10 output "HDMI-A-1"
       ''
     ];
   };
