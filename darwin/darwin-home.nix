f774a8a68cc23a764fb36d44c0c07e98d700535d
Sam Bossley <bossley.samuel@gmail.com>
2023-05-11 19:20:38 -0700

    feat: link all darwin home configuration files

--- darwin/darwin-home.nix
+++ darwin/darwin-home.nix
@@ -14,6 +14,13 @@ in
   home-manager.users."${username}" = {
     home.file.".config/kitty/kitty.conf".source = ../user/config/kitty/kitty.conf;
 
+    home.file.".config/rg/rgrc".source = ../user/config/rg/rgrc;
+    home.file.".config/bat/config".source = ../user/config/bat/config;
+
+    home.file.".config/nvim".source = ../user/config/nvim;
+
+    home.file.".config/git/config".source = ../user/config/git/config;
+
     home.stateVersion = "22.05";
   };
 }
