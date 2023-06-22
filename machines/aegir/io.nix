fdcf72db261ce7a69cf61e2ab0201b42aa241e29
Sam Bossley <bossley.samuel@gmail.com>
2023-06-22 07:11:25 -0700

    feat: add nm-connection-editor to aegir

--- machines/aegir/io.nix
+++ machines/aegir/io.nix
@@ -7,10 +7,16 @@ in
 {
   imports = [ home-manager.nixosModules.home-manager ];
 
-  home-manager.users."${username}".home.file.".config/sway/config" = lib.mkForce {
-    text = builtins.concatStringsSep "\n" [
-      (builtins.readFile ../../user/config/sway/config)
-      (builtins.readFile ./swayconfig)
+  home-manager.users."${username}" = {
+    home.packages = with pkgs; [
+      networkmanagerapplet
     ];
+
+    home.file.".config/sway/config" = lib.mkForce {
+      text = builtins.concatStringsSep "\n" [
+        (builtins.readFile ../../user/config/sway/config)
+        (builtins.readFile ./swayconfig)
+      ];
+    };
   };
 }
