5f308c95cb5119f6b3fbe95846f0e47eeb873af7
Sam Bossley <bossley.samuel@gmail.com>
2023-07-30 11:40:57 -0400

    feat(nvim): refactor formatter to lua

--- user/config/nvim/lua/formatter.lua
+++ user/config/nvim/lua/formatter.lua
@@ -1,41 +1,40 @@
-vim.cmd([[
+local formattingEnabled = true
 
-let s:formatting_enabled = 1
+vim.g.SetFormat = function(value) formattingEnabled = value end
 
-fu! g:SetFormat(value)
-  let s:formatting_enabled = a:value
-endfunction
+vim.api.nvim_create_user_command('FormatOn',
+                                 function() vim.g.SetFormat(true) end,
+                                 {nargs = 0})
+vim.api.nvim_create_user_command('FormatOff',
+                                 function() vim.g.SetFormat(false) end,
+                                 {nargs = 0})
 
-com! -nargs=0 FormatOn call g:SetFormat(1)
-com! -nargs=0 FormatOff call g:SetFormat(0)
+vim.g.Format = function()
+    local ft = vim.bo.filetype
+    local file = vim.fn.expand('%:p')
+    -- step 1: silently format the file in place (silent !cmd expand(%:p))
+    if ft == 'c' or ft == 'cpp' then
+        vim.cmd("exe 'silent !clang-format -i " .. file .. "'")
+    elseif ft == 'go' then
+        vim.cmd("exe 'silent !gofmt -w " .. file .. "'")
+    elseif ft == 'kotlin' then
+        vim.cmd("exe 'silent !ktlint -F " .. file .. "'")
+    elseif ft == 'lua' then
+        vim.cmd("exe 'silent !lua-format -i " .. file .. "'")
+    elseif ft == 'nix' then
+        vim.cmd("exe 'silent !nixpkgs-fmt " .. file .. "'")
+    elseif ft == 'rust' then
+        vim.cmd("exe 'silent !rustfmt " .. file .. "'")
+    end
+    -- step 2: reload formatting changes
+    vim.cmd 'e'
+    -- step 3: reload syntax highlighting
+    vim.cmd 'redraw'
+    -- step 4: reopen the currently closed fold
+    vim.cmd 'norm! zv'
+end
 
-fu! g:Format()
-  let ft = &ft
-  " step 1: silently format the file in place (silent !cmd expand(%:p))
-  if ft == "c" || ft =="cpp"
-    exe 'silent !clang-format -i ' . expand('%:p')
-  elseif ft == "go"
-    exe 'silent !gofmt -w ' . expand('%:p')
-  elseif ft == "kotlin"
-    exe 'silent !ktlint -F ' . expand('%:p')
-  elseif ft == "lua"
-    exe 'silent !lua-format -i ' . expand('%:p')
-  elseif ft == "nix"
-    exe 'silent !nixpkgs-fmt ' . expand('%:p')
-  elseif ft == "rust"
-    exe 'silent !rustfmt ' . expand('%:p')
-  en
-  " step 2: reload formatting changes
-  e
-  " step 3: reload syntax highlighting
-  redraw
-  " step 4: reopen the currently closed fold
-  norm! zv
-endfunction
-
-augroup formatter
-  au!
-  au BufWritePost * if s:formatting_enabled | call g:Format() | en
-augroup end
-
-]])
+vim.api.nvim_create_autocmd('BufWritePost', {
+    pattern = '*',
+    callback = function() if formattingEnabled then vim.g.Format() end end
+})
