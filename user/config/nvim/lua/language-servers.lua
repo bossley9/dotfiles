3b0490b0b179b32fc483b7892e86e78f208541ac
Sam Bossley <bossley.samuel@gmail.com>
2023-08-01 16:25:29 -0400

    fix(darwin): display Font Awesome icons in terminal

--- user/config/nvim/lua/language-servers.lua
+++ user/config/nvim/lua/language-servers.lua
@@ -51,7 +51,7 @@ vim.diagnostic.config({
     signs = true,
     underline = true,
     update_in_insert = false,
-    severity_sort = false
+    severity_sort = true
 })
 
 local signs = { Error = "", Warn = "", Hint = "", Info = "" }
